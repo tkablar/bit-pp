f64b02712265aa61e8ae16ee2eae1356b91b5227
tkablar <kablartamara@yahoo.com>
2022-10-04 20:10:01 +0200

    js files

---
+++ variables.js
@@ -0,0 +1,6 @@
+var a;
+console.log(a);
+a = 4;
+console.log(a);
+a = "test";
+console.log(a);
---
+++ variablesEx.js
@@ -0,0 +1,19 @@
+var someTextV;
+console.log(someTextV);
+someTextV = 7;
+console.log(someTextV)
+var SomeTextV;
+SomeTextV = 5;
+console.log(SomeTextV);
+var asNumber;
+console.log(asNumber);
+var linkAndLists;
+console.log(linkAndLists);
+var ContactIndex;
+console.log(ContactIndex);
+
+
+var Hello = "Today is a good day and it was raining.";
+console.log(Hello);
+
+
